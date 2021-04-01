71bedcfcbbde2880783c76aa236313c49f68dc4b
CasedG <77801950+CasedG@users.noreply.github.com>
2021-04-01 18:33:13 -0400

    Update systemInformation.ps1
    added functions to lab 4

--- powershell/systemInformation.ps1
+++ powershell/systemInformation.ps1
@@ -1,69 +1,96 @@
 ﻿""
 ""
-"System Hardware Description" 
-$systemHardwareDescription = Get-WmiObject -class win32_computersystem | fl
-$systemHardwareDescription
+""
+function systemHardware {
+    "System Hardware Description" 
+    $systemHardwareDescription = Get-WmiObject -class win32_computersystem | fl
+    $systemHardwareDescription
+    }
+systemHardware
 ""
 ""
-"OS Name and Version Number"
-$osNameandVersion = Get-WmiObject -class win32_operatingsystem | fl Name, Version
-$osNameandVersion
-""
-""
-"Processor Description"
-$processorDescription = Get-WmiObject -class win32_processor | fl Description, NumberOfCores, L2CacheSize, L3CacheSize 
-$processorDescription
-""
-""
-"Summary of RAM"
-$totalCapacity = 0
 
-Get-WmiObject -class win32_physicalmemory |
-    foreach {
-        New-Object -TypeName psobject -Property @{
-            Vendor = $_.manufacturer
-            Description = $_.description
-            "Size(MB)" = $_.capacity/1MB
-            Bank = $_.banklabel
-            Slot = $_.devicelocator
-        }
-        $totalCapacity += $_.capacity/1mb
-    } |
-    format-table -AutoSize Vendor,
-                           Description,
-                           "Size(MB)",
-                           Bank,
-                           Slot
+function osNameVersion {
+    "OS Name and Version Number"
+    $osNameandVersion = Get-WmiObject -class win32_operatingsystem | fl Name, Version
+    $osNameandVersion
+    }
+osNameVersion
+""
+""
 
-"Total RAM: ${totalCapacity}MB"
+function processorDescription {
+    "Processor Description"
+    $processorDescription = Get-WmiObject -class win32_processor | fl Description, NumberOfCores, L2CacheSize, L3CacheSize 
+    $processorDescription
+    }
+processorDescription
 ""
 ""
-""
-""
-"Summary of disk drives"
-$diskdrives = get-ciminstance cim_diskdrive
 
-foreach ($disk in $diskdrives) {
-    $partitions = $disk | Get-CimAssociatedInstance -resultclassname CIM_diskpartition
-    foreach ($partition in $partitions) {
-        $logicaldisks = $partition | Get-CimAssociatedInstance -ResultClassName CIM_logicaldisk
-        foreach ($logicaldisk in $logicaldisks) {
-            New-Object -TypeName psobject -property @{Manufacturer=$disk.Manufacturer
-                                                      Location=$partition.deviceid
-                                                      Drive=$logicaldisk.deviceid
-                                                      "Size(GB)"=$logicaldisk.size / 1gb -as [int]
-                                                      }
+function summaryRAM {
+    "Summary of RAM"
+    $totalCapacity = 0
+
+    Get-WmiObject -class win32_physicalmemory |
+        foreach {
+            New-Object -TypeName psobject -Property @{
+                Vendor = $_.manufacturer
+                Description = $_.description
+                "Size(MB)" = $_.capacity/1MB
+                Bank = $_.banklabel
+                Slot = $_.devicelocator
+            }
+            $totalCapacity += $_.capacity/1mb
+        } |
+        format-table -AutoSize Vendor,
+                              Description,
+                               "Size(MB)",
+                               Bank,
+                               Slot
+
+    "Total RAM: ${totalCapacity}MB"
+    }
+summaryRAM
+""
+""
+""
+""
+
+function diskDrives {
+    "Summary of disk drives"
+    $diskdrives = get-ciminstance cim_diskdrive
+    foreach ($disk in $diskdrives) {
+        $partitions = $disk | Get-CimAssociatedInstance -resultclassname CIM_diskpartition
+        foreach ($partition in $partitions) {
+            $logicaldisks = $partition | Get-CimAssociatedInstance -ResultClassName CIM_logicaldisk
+            foreach ($logicaldisk in $logicaldisks) {
+                New-Object -TypeName psobject -property @{Manufacturer=$disk.Manufacturer
+                                                          Location=$partition.deviceid
+                                                          Drive=$logicaldisk.deviceid
+                                                          "Size(GB)"=$logicaldisk.size / 1gb -as [int]
+                                                          }
+            }
         }
     }
-}
+    }
+diskDrives
 ""
 ""
 ""
-"Network Adapter Configuration"
-get-ciminstance win32_networkadapterconfiguration | where-object ipenabled -eq "True" | format-table Description, Index, IPaddress, IPsubnet, DNSDomain, DNSHostName 
+
+function networkAdapter {
+    "Network Adapter Configuration"
+    get-ciminstance win32_networkadapterconfiguration | where-object ipenabled -eq "True" | format-table Description, Index, IPaddress, IPsubnet, DNSDomain, DNSHostName 
+    }
+networkAdapter
 ""
 ""
 ""
-"Video Card Summary"
-get-wmiobject -class win32_videocontroller | fl Name, Description, CurrentHorizontalResolution, CurrentVerticalResolution
-$videoSummary = Get-WmiObject -class win32_videocontroller
+function videoCard {
+    "Video Card Summary"
+    get-wmiobject -class win32_videocontroller | fl Name, Description, CurrentHorizontalResolution, CurrentVerticalResolution
+    $videoSummary = Get-WmiObject -class win32_videocontroller
+    }
+videoCard
+
